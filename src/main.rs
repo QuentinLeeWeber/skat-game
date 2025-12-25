9ffbbc75560002ec4547ea5ca43f6bbd6db1ed93
Quentin Weber <q.weber@campus.tu-berlin.de>
2025-12-25 15:19:14 +0100

    feat(ui): add card textures

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,22 @@ async fn main() -> Result<(), slint::PlatformError> {
     let app = MainWindow::new()?;
 
     let hand_model = Rc::new(VecModel::from(Vec::<Card>::new()));
-    app.set_hand(ModelRc::from(hand_model.clone()));
+    app.set_hand(ModelRc::from(Rc::clone(&hand_model)));
 
+    let hm = Rc::clone(&hand_model);
     app.on_play_card(move |card| {
         println!("Playing card: {:?}", card);
-        let index = hand_model.iter().position(|c| c == card);
+        let index = hm.iter().position(|c| c == card);
         if let Some(i) = index {
-            hand_model.remove(i);
+            hm.remove(i);
         }
+    });
+
+    let hm = Rc::clone(&hand_model);
+    app.on_set_position(move |from, to| {
+        println!("Moving card from {} to {}", from, to);
+        let card = hm.remove(from as usize);
+        hm.insert(to as usize, card);
     });
 
     let weak_app = app.as_weak();
@@ -29,11 +37,11 @@ async fn main() -> Result<(), slint::PlatformError> {
 async fn main_loop(app: Weak<MainWindow>) -> Result<(), slint::PlatformError> {
     let cards_to_add = vec![
         Card {
-            suit: CardSuit::Hearts,
+            suit: CardSuit::Heart,
             rank: CardRank::Seven,
         },
         Card {
-            suit: CardSuit::Diamonds,
+            suit: CardSuit::Diamond,
             rank: CardRank::Eight,
         },
         Card {
@@ -41,8 +49,8 @@ async fn main_loop(app: Weak<MainWindow>) -> Result<(), slint::PlatformError> {
             rank: CardRank::Nine,
         },
         Card {
-            suit: CardSuit::Clubs,
-            rank: CardRank::Nine,
+            suit: CardSuit::Spade,
+            rank: CardRank::Ace,
         },
     ];
 
