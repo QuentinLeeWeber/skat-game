d0bbb629c6220beeb6253ea8480a203624e06bd1
Quentin Weber <mail@quentman.de>
2026-01-14 03:08:36 +0100

    remove server.conf from git

--- client/build.rs
+++ client/build.rs
@@ -1,3 +1,8 @@
+use std::fs;
+
 fn main() {
+    fs::read_to_string("server.conf")
+        .expect("server.conf does not exist. In client root should exists a server.conf file. With form: {SERVER_IP}:[SERVER_PORT}");
+
     slint_build::compile("ui/app-window.slint").expect("Slint build failed");
 }
